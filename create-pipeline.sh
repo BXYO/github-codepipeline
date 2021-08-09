1b7e64b0ab046e1c5ee6eb3212cd7f3fc8b05202
BXYO <80680275+BXYO@users.noreply.github.com>
2021-08-09 23:17:40 +0530

    Update create-pipeline.sh

--- create-pipeline.sh
+++ create-pipeline.sh
@@ -1,15 +1,4 @@
-#!/bin/bash
 
-# *** Change this to the desired name of the Cloudformation stack of 
-# your Pipeline (*not* the stack name of your app)
-CODEPIPELINE_STACK_NAME="github-codepipeline"
-
-if [ -z ${1} ]
-then
-	echo "PIPELINE CREATION FAILED!"
-        echo "Pass your Github OAuth token as the first argument"
-	exit 1
-fi
 
 set -eu
 
